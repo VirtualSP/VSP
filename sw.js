8399d08bf9c582c97e4495608cfbe07b26c12cac
VirtualSP <masami_amano@hotmail.com>
2020-02-17 14:09:13 +0900

    Feb. 2020 PWA

---
+++ sw.js
@@ -0,0 +1,69 @@
+var CACHE_NAME = 'VSPcache1944';
+var urlsToCache = [
+    '/VirtualSPD/index.html',
+    '/VirtualSPD/js/app66.js',
+    '/VirtualSPD/js/three.min.js',
+    '/VirtualSPD/js/musicmetadata.js',
+ 
+    '/VirtualSPD/icon_192.png',
+     '/VirtualSPD/icon_512.png'
+];
+
+self.addEventListener('install', (e) => {
+	console.log('[Service Worker] Install');
+	e.waitUntil(
+	caches.open(CACHE_NAME).then((cache) => {
+          		console.log('[Service Worker] Caching all: app shell and content');
+      	return cache.addAll(urlsToCache)
+	.then(() => {
+          console.info('All files are cached');
+          return self.skipWaiting(); //To forces the waiting service worker to become the active service worker
+        })
+    })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(caches
+        .match(event.request)
+        .then(function(response) {
+            return response ? response : fetch(event.request);
+        })
+    );
+});
+
+
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    (function() {
+      caches.keys().then(function(oldCacheKeys) {
+        oldCacheKeys
+          .filter(function(key) {
+            return key !== CACHE_NAME;
+          })
+          .map(function(key) {
+            return caches.delete(key);
+          });
+      });
+      clients.claim();
+    })()
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    (function() {
+      caches.keys().then(function(oldCacheKeys) {
+        oldCacheKeys
+          .filter(function(key) {
+            return key !== CACHE_NAME;
+          })
+          .map(function(key) {
+            return caches.delete(key);
+          });
+      });
+      clients.claim();
+    })()
+  );
+});
